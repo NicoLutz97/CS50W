71b989a8ed41880b09c4bbc61ef24645503e27e2
NicoLutz97 <124193486+NicoLutz97@users.noreply.github.com>
2024-04-01 14:02:04 +0000

    new

--- week4/project2/commerce/auctions/models.py
+++ week4/project2/commerce/auctions/models.py
@@ -4,28 +4,3 @@ from django.db import models
 
 class User(AbstractUser):
     pass
-
-
-class Listing(models.Model):
-    CHOICES_CATEGORY = [
-        ("electronics", "Electronics"),
-        ("fashion", "Fashion"),
-        ("sports_hobbys", "Sports and Hobbys"),
-        ("house_garden", "House and Garden"),
-        ("books_movies_music", "Books, Movies, Music"),
-        ("others", "Others"),
-    ]
-    
-
-    title = models.CharField(max_length=64, blank=False, null=False)
-    description = models.TextField(blank=False, null=False)
-    starting_bid = models.DecimalField(max_digits=10, decimal_places=2, blank=False, null=False)
-    image = models.URLField(help_text="Insert URL for prodcut image here")
-    category = models.CharField(
-        max_length=64,
-        choices=CHOICES_CATEGORY,
-        default="others"
-        )
-    
-    def __str__(self) -> str:
-        return self.title
